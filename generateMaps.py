3cb9cb960ce4e4be047c192f6a259e67ad7e8f45
Shreepad Patil <shreepad007@gmail.com>
2016-02-15 01:43:36 -0500

    Initial Draft
    Initial Draft of callers

---
+++ generateMaps.py
@@ -0,0 +1,16 @@
+'''
+Created on 07-Feb-2016
+
+@author: Shreepad Patil
+@Ruid: 169005770
+'''
+
+import grid
+import pickle
+
+for i in range(1,51):
+    g = grid.grid(101,5,5,1)
+    g.generate()
+    f = open('Map' + str(i), 'wb+')
+    pickle.dump(g,f)
+    f.close()
